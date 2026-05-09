fc739797508367df16683d15d609c3bb7ad04858
Michael Grossberg <mgrossberg@ccny.cuny.edu>
2026-05-08 23:36:23 -0400

    fix(ci): set correct github pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/tmux-trek/" : "/",
   server: {
     port: 4173,
   },
   build: {
     chunkSizeWarningLimit: 2000,
   },
-});
+}));
